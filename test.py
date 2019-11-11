a6a8e8e1735304837d8c04dc16b538e8fbb2f009
syk1k <dambemondo@gmail.com>
2019-11-11 02:49:41 +0000

    Auth test

--- test.py
+++
@@ -1,6 +0,0 @@
-from pydrive.auth import GoogleAuth
-
-
-gauth = GoogleAuth()
-gauth.LoadClientConfigFile('media/credentials/client_secrets.json')
-gauth.LocalWebserverAuth(host_name='localhost', port_numbers=[8080])
